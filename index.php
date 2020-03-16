0063f2619eea2e71c42e3c661c67865966cc75c2
Javier <xavier.mcin@gmail.com>
2020-03-15 21:22:45 -0400

    working

--- index.php
+++ index.php
@@ -38,10 +38,16 @@
 
                             <div>
                                 <img src="img/first-pic2.jpg" alt="table of people eating">
+
+                                <div>
+                                    <p>Table of People</p>
+                                    <p>Mar.15.2020</p>
+                                </div>
+
                             </div>
 
-                            <h2>There are many stores that worry about beef</h2>
-                            <p>BY PAUL DILLYO Mar 15, 2020</p>
+                            <h3>There are many stores that worry about beef</h3>
+                            <p>BY PAULA DILLYO Mar 15, 2020</p>
                             <p>Read More</p>
 
                         </div>
@@ -50,10 +56,15 @@
 
                             <div>
                                 <img src="img/first-pic3.jpg" alt="picture of nice juicy hamburger">
+
+                                <div>
+                                    <p>Delcious Looking Burger <br> <span>In Studio</span></p>
+                                    <p>Mar.15.2020</p>
+                                </div>
                             </div>
 
-                            <h2>There are many stores that worry about beef</h2>
-                            <p>BY PAUL DILLYO Mar 15, 2020</p>
+                            <h3>There are many stores that worry about beef</h3>
+                            <p>BY PAULA DILLYO Mar 15, 2020</p>
                             <p>Read More</p>
 
                         </div>
@@ -63,9 +74,100 @@
                 </div>
             </section>   
 
-            <div class="main-right"></div>
 
-            <div class="main-left"></div>
+
+
+
+
+
+
+            <section id="second">
+                <div class="second-container container">
+
+                    <div class="main-left">
+
+                        <!-- ARTICLES -->
+                        <div class="articles">
+                            <h2>Latest <span>Articles</span></h2>
+
+                            <div>
+
+                                <div class="arts">
+                                    <div>
+                                        <img src="img/articles-pic1.jpg" alt="chef preparing to spead pizza dough">
+
+                                        <div>
+                                            <p>Chef Flouring Hand Before Rolling <br> <span>Pizza</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                                <div class="arts">
+                                    <div>
+                                        <img src="img/articles-pic2.jpg" alt="chef tasting dish">
+
+                                        <div>
+                                            <p>Chef Taste Testing <br> <span>Dish</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                                <div class="arts">
+                                    <div>
+                                        <img src="img/articles-pic3.jpg" alt="young women eating">
+
+                                        <div>
+                                            <p>Young Wome Sitting Down <br> <span>Eating</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+                    <!-- RIGHT COLUMN -->
+
+                    <div class="main-right">
+                        <div class="must-read">
+
+                            <h2>Must <span>Read</span></h2>
+
+                            <div>
+                                <div class="reads"></div>
+                                <div class="reads"></div>
+                                <div class="reads"></div>
+                                <div class="reads"></div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
 
         </main>
 
