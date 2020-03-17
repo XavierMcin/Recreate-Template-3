cd827b8e622e5b5fe25bc92a95815dfbc1e1ed91
Javier <xavier.mcin@gmail.com>
2020-03-16 22:56:29 -0400

    working on videos

--- index.php
+++ index.php
@@ -8,13 +8,35 @@
         <![endif]-->
 
 
+
+
+
+
+        <!-- ###### -->
         <!-- HEADER -->
+        <!-- ###### -->
+
+
+
+
+
         <?php include "includes/header.php"; ?>
 
         <!-- <?php include "includes/navigation.php"; ?> -->
 
 
+
+
+
+
+        <!-- ############ -->
         <!-- MAIN SECTION -->
+        <!-- ############ -->
+
+
+
+
+
         <main>
         
             <section id="first">
@@ -84,7 +106,18 @@
             <section id="second">
                 <div class="second-container container">
 
+
+
+
+                    <!-- ########### -->
                     <!-- LEFT COLUMN -->
+                    <!-- ########### -->
+
+
+
+
+
+
                     <div class="main-left">
 
                         <!-- ARTICLES -->
@@ -282,6 +315,125 @@
 
 
 
+
+                        <!-- STREET FOOD -->
+
+
+                        <div class="street-food">
+                            <h2>Street <span>Food</span></h2>
+
+                            <div>
+
+                                <div class="street">
+                                    <div>
+                                        <img src="img/articles-pic1.jpg" alt="chef preparing to spead pizza dough">
+
+                                        <div>
+                                            <p>Chef Flouring Hand Before Rolling <br> <span>Pizza</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                                <div class="street">
+                                    <div>
+                                        <img src="img/articles-pic2.jpg" alt="chef tasting dish">
+
+                                        <div>
+                                            <p>Chef Taste Testing <br> <span>Dish</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                                <div class="street">
+                                    <div>
+                                        <img src="img/articles-pic3.jpg" alt="young women eating">
+
+                                        <div>
+                                            <p>Young Wome Sitting Down <br> <span>Eating</span></p>
+                                            <p>Mar.15.2020</p>
+                                        </div>
+                                    </div>
+
+                                    <h3>There are many stores that worry about beef</h3>
+                                    <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+                        <!-- HEALTH AND LIFESTYLES -->
+
+                        <div class="lifestyles">
+
+                            <h2>Health And <span>Lifestyles</span></h2>
+
+                            <div>
+
+                                <div class="life">
+
+                                    <div>
+                                        <img src="img/health-pic1.jpg" alt="">
+                                    </div>
+
+                                    <div>
+                                        <h3>There Are Countless Ways To Stay Healty</h3>
+                                        <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia molestiae repellat praesentium obcaecati asperiores provident?</p>
+                                        <p>Read More</p>
+                                    </div>
+
+                                </div>
+
+                                <div class="life">
+
+                                    <div>
+                                        <img src="img/health-pic2.jpg" alt="">
+                                    </div>
+
+                                    <div>
+                                        <h3>There Are Countless Ways To Stay Healty</h3>
+                                        <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia molestiae repellat praesentium obcaecati asperiores provident?</p>
+                                        <p>Read More</p>
+                                    </div>
+
+                                </div>
+
+                                <div class="life">
+
+                                    <div>
+                                        <img src="img/health-pic3.jpg" alt="">
+                                    </div>
+
+                                    <div>
+                                        <h3>There Are Countless Ways To Stay Healty</h3>
+                                        <div class="author"><span><img src="img/paula.jpg" alt="author"></span> <p>By Paula Dillyo Mar 15, 2020</p></div>
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia molestiae repellat praesentium obcaecati asperiores provident?</p>
+                                        <p>Read More</p>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
                     <!-- LEFT COLUMN END -->
                     </div>
 
@@ -295,7 +447,17 @@
 
 
 
+
+
+
+
+
+
+                    <!-- ############ -->
                     <!-- RIGHT COLUMN -->
+                    <!-- ############ -->
+
+
 
                     <div class="main-right">
 
@@ -392,6 +554,48 @@
                         </div>
 
 
+
+
+                        <!-- RESERVATION -->
+
+                        <div class="reservation">
+                            <img src="img/reservation.jpg" alt="">
+                            <h3>Konoha Leaf</h3>
+                            <p>Make <br>Reservations</p>
+                        </div>
+
+
+
+                        <!-- LATEST VIDEOS -->
+
+                        <div class="videos">
+
+                            <h2>Latest <span>Videos</span></h2>
+
+                            <div>
+
+                                <div class="vids">
+                                    <div><img src="" alt=""></div>
+                                    <h3>A New Report Shows The Deep Frying Isn't All That Bad</h3>
+                                </div>
+
+                                <div class="vids">
+                                    <div><img src="" alt=""></div>
+                                    <h3>Having A Diverse Menu Is Very Cool To Have</h3>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
+
+
+
+
+
+
                     <!-- RIGHT COLUMN END -->    
                     </div>
 
@@ -401,7 +605,22 @@
         </main>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- ###### -->
         <!-- FOOTER -->
+        <!-- ###### -->
         <footer></footer>
 
 
