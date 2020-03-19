123bee1577c06967cfdfabf957df94295a2eb64d
Javier <xavier.mcin@gmail.com>
2020-03-19 17:19:32 -0400

    finshed index page

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,75 @@
 <footer>
     <div class="footer-container">
 
+        <div class="foots">
+
+            <h3>About WebFoodies</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sint facere nulla ullam soluta natus optio, laboriosam totam reprehenderit aliquam, laudantium minima similique repellendus voluptas voluptatibus, est nemo vitae? Unde!</p>
+
+            <div class="social">
+                <div class="facebook"><span class="fab fa-facebook-f"></span></div>
+                <div class="linkedin"><span class="fab fa-linkedin-in"></span></div>
+                <div class="twitter"><span class="fab fa-twitter"></span></div>
+                <div class="googlep"><span class="fab fa-google-plus-g"></span></div>
+            </div>
+
+        </div>
+
+        <div class="foots">
+
+            <h3>Editor Pics</h3>
+
+            <div class="ePics">
+                <div>
+                    <img src="img/reads-pic1.jpg" alt="">
+                </div>
+                <div>
+                    <h3>Some of The Editors Favorite Photos That Are On The Website</h3>
+                    <p>Mar 15, 2020</p>
+                </div>
+            </div>
+
+            <div class="ePics">
+                <div>
+                    <img src="img/reads-pic1.jpg" alt="">
+                </div>
+                <div>
+                    <h3>Some of The Editors Favorite Photos That Are On The Website</h3>
+                    <p>Mar 15, 2020</p>
+                </div>
+            </div>
+
+            <div class="ePics">
+                <div>
+                    <img src="img/reads-pic1.jpg" alt="">
+                </div>
+                <div>
+                    <h3>Some of The Editors Favorite Photos That Are On The Website</h3>
+                    <p>Mar 15, 2020</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="foots">
+
+            <h3>Information</h3>
+
+            <div class="links">
+                <ul>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Street Food</a></li>
+                    <li><a href="#">Practices</a></li>
+                    <li><a href="#">Popular</a></li>
+                    <li><a href="#">Dishes</a></li>
+                    <li><a href="#">Lifestyles</a></li>
+                    <li><a href="#">Cultures</a></li>
+                    <li><a href="#">Comments</a></li>
+                </ul>
+            </div>
+
+        </div>
+
     </div>
 
     <div class="bFooter">
